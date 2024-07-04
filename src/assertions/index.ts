5f4c1e1153bf002d434d0f4bdab9fa8a4ff4a301
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-07-04 13:04:08 -0700

    move all functions

--- src/assertions/index.ts
+++ src/assertions/index.ts
@@ -1237,6 +1237,105 @@ function costAssertion(
   };
 }
 
+function isValidOpenAiToolsCallAssertion(
+  outputString: string,
+  renderedValue: AssertionValue | undefined,
+  inverse: boolean,
+  assertion: Assertion,
+  test: AtomicTestCase,
+  output: object | string,
+  provider: ApiProvider | undefined,
+) {
+  const toolsOutput = output as {
+    type: 'function';
+    function: { arguments: string; name: string };
+  }[];
+  if (
+    !Array.isArray(toolsOutput) ||
+    toolsOutput.length === 0 ||
+    typeof toolsOutput[0].function.name !== 'string' ||
+    typeof toolsOutput[0].function.arguments !== 'string'
+  ) {
+    return {
+      pass: false,
+      score: 0,
+      reason: `OpenAI did not return a valid-looking tools response: ${JSON.stringify(
+        toolsOutput,
+      )}`,
+      assertion,
+    };
+  }
+
+  try {
+    toolsOutput.forEach((toolOutput) =>
+      validateFunctionCall(
+        toolOutput.function,
+        (provider as OpenAiChatCompletionProvider).config.tools?.map((tool) => tool.function),
+        test.vars,
+      ),
+    );
+    return {
+      pass: true,
+      score: 1,
+      reason: 'Assertion passed',
+      assertion,
+    };
+  } catch (err) {
+    return {
+      pass: false,
+      score: 0,
+      reason: (err as Error).message,
+      assertion,
+    };
+  }
+}
+
+function isValidOpenAiFunctionCallAssertion(
+  outputString: string,
+  renderedValue: AssertionValue | undefined,
+  inverse: boolean,
+  assertion: Assertion,
+  test: AtomicTestCase,
+  output: object | string,
+  provider: ApiProvider | undefined,
+) {
+  const functionOutput = output as { arguments: string; name: string };
+  if (
+    typeof functionOutput !== 'object' ||
+    typeof functionOutput.name !== 'string' ||
+    typeof functionOutput.arguments !== 'string'
+  ) {
+    return {
+      pass: false,
+      score: 0,
+      reason: `OpenAI did not return a valid-looking function call: ${JSON.stringify(
+        functionOutput,
+      )}`,
+      assertion,
+    };
+  }
+  try {
+    validateFunctionCall(
+      functionOutput,
+      (provider as OpenAiChatCompletionProvider).config.functions,
+      test.vars,
+    );
+    return {
+      pass: true,
+      score: 1,
+      reason: 'Assertion passed',
+      assertion,
+    };
+  } catch (err) {
+    return {
+      pass: false,
+      score: 0,
+      reason: (err as Error).message,
+      assertion,
+    };
+  }
+}
+
 export async function runAssertion({
   prompt,
   provider,
@@ -1256,9 +1355,7 @@ export async function runAssertion({
   logProbs?: number[];
   cost?: number;
 }): Promise<GradingResult> {
-  let pass: boolean = false;
-  let score: number = 0.0;
-
+  
   invariant(assertion.type, `Assertion must have a type: ${JSON.stringify(assertion)}`);
 
   const inverse = assertion.type.startsWith('not-');
@@ -1384,86 +1481,27 @@ export async function runAssertion({
   }
 
   if (baseType === 'is-valid-openai-tools-call') {
-    const toolsOutput = output as {
-      type: 'function';
-      function: { arguments: string; name: string };
-    }[];
-    if (
-      !Array.isArray(toolsOutput) ||
-      toolsOutput.length === 0 ||
-      typeof toolsOutput[0].function.name !== 'string' ||
-      typeof toolsOutput[0].function.arguments !== 'string'
-    ) {
-      return {
-        pass: false,
-        score: 0,
-        reason: `OpenAI did not return a valid-looking tools response: ${JSON.stringify(
-          toolsOutput,
-        )}`,
-        assertion,
-      };
-    }
-
-    try {
-      toolsOutput.forEach((toolOutput) =>
-        validateFunctionCall(
-          toolOutput.function,
-          (provider as OpenAiChatCompletionProvider).config.tools?.map((tool) => tool.function),
-          test.vars,
-        ),
-      );
-      return {
-        pass: true,
-        score: 1,
-        reason: 'Assertion passed',
-        assertion,
-      };
-    } catch (err) {
-      return {
-        pass: false,
-        score: 0,
-        reason: (err as Error).message,
-        assertion,
-      };
-    }
+    return isValidOpenAiToolsCallAssertion(
+      outputString,
+      renderedValue,
+      inverse,
+      assertion,
+      test,
+      output,
+      provider,
+    );
   }
 
   if (baseType === 'is-valid-openai-function-call') {
-    const functionOutput = output as { arguments: string; name: string };
-    if (
-      typeof functionOutput !== 'object' ||
-      typeof functionOutput.name !== 'string' ||
-      typeof functionOutput.arguments !== 'string'
-    ) {
-      return {
-        pass: false,
-        score: 0,
-        reason: `OpenAI did not return a valid-looking function call: ${JSON.stringify(
-          functionOutput,
-        )}`,
-        assertion,
-      };
-    }
-    try {
-      validateFunctionCall(
-        functionOutput,
-        (provider as OpenAiChatCompletionProvider).config.functions,
-        test.vars,
-      );
-      return {
-        pass: true,
-        score: 1,
-        reason: 'Assertion passed',
-        assertion,
-      };
-    } catch (err) {
-      return {
-        pass: false,
-        score: 0,
-        reason: (err as Error).message,
-        assertion,
-      };
-    }
+    return isValidOpenAiFunctionCallAssertion(
+      outputString,
+      renderedValue,
+      inverse,
+      assertion,
+      test,
+      output,
+      provider,
+    );
   }
 
   if (baseType === 'javascript') {
